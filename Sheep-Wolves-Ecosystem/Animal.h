884171ea91013517dc310291500615eca205d34b
Raffallo <gierczak.rafal@o2.pl>
2025-03-27 20:20:17 +0100

    *changed: size of the Animal struct

--- Sheep-Wolves-Ecosystem/Animal.h
+++ Sheep-Wolves-Ecosystem/Animal.h
@@ -1,5 +1,8 @@
 #pragma once
 
+//for better std::vector optimizations (bit-packing) we could use bool directly instead of `struct Animal`
+
+#pragma pack(push, 1)
 struct Animal
 {
     Animal(bool isMale) : isMaleGender(isMale) { };
@@ -7,3 +10,4 @@ struct Animal
 
     bool isMaleGender{ false };
 };
+#pragma pack(pop)
